e120f5bee5e39a7f406f2ba0a00975ed6133461f
root <root@ip-10-130-30-222.ap-southeast-2.compute.internal>
2016-01-04 05:50:30 +0000

    Bug fixes

---
+++ app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_t4h1ug',
+  ),
+  'install' => 
+  array (
+    'date' => 'Sun, 03 Jan 2016 08:31:01 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'f4b82448cae966a73fbf58abc3ee1fbb',
+  ),
+  'session' => 
+  array (
+    'save' => 'db',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+);
